d114830224b7b3d705f83a547ed090561f62473b
faha89 <fabian-hansen@web.de>
2020-11-23 13:24:59 +0100

    Timer überarbeitet
    Mediaplayer überarbeitet

--- app/src/main/java/de/hsf/jffh/l02responsive_design/MainActivity.java
+++ app/src/main/java/de/hsf/jffh/l02responsive_design/MainActivity.java
@@ -23,9 +23,11 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
 
-    public static final String TAG = "hsfMainActivity";    //Erzeugt TAG für logs
+public class MainActivity extends AppCompatActivity {
+    //Erzeugt TAG für logs
+    public static final String TAG = "hsfMainActivity";
 
     private Button buttonStart;
     private EditText editTextStartzeit;
@@ -34,11 +36,15 @@ public class MainActivity extends AppCompatActivity {
     private CountDownTimer countDownTimer;
     private String strTime;
     private MediaPlayer mediaPlayer;
-
+    /*
+    private String buttonStatus; // TODO switch case für Status des Buttons
+    public static final String stringButtonStart = getResources().getString(R.string.button_Start);
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate()");        //Log für Methodenaufruf
+        //Log für Methodenaufruf
+        Log.d(TAG, "onCreate()");
 
         setContentView(R.layout.activity_main);
         //setzt Screen Orientation auf Portrait in der Manifest
@@ -48,12 +54,12 @@ public class MainActivity extends AppCompatActivity {
         editTextStartzeit = (EditText) findViewById(R.id.editTextStartzeit);
         textViewZeit = (TextView) findViewById(R.id.textViewZeit);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-        buttonStart.setOnClickListener(new View.OnClickListener() {            //Erstellt listener für Buttonklick
+        //Erstellt listener für Buttonklick
+        buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "buttonStart.OnClickListener()");
                 strTime = editTextStartzeit.getText().toString();
-
                 //checkt, ob input leer ist und gibt feedback
                 if (strTime.isEmpty()) {
                     Log.d(TAG, "empty input");
@@ -61,38 +67,36 @@ public class MainActivity extends AppCompatActivity {
                     progressBar.setProgress(0);
                 } else {
                     if (getResources().getString(R.string.button_Start).equals(buttonStart.getText())) {
+                    /*
+                    switch (buttonStatus) { //TODO switch case für Status des Buttons (start, stop, stop alarm)
+                        case (stringButtonStart) :
+                     */
                         progressBar.setMax(Integer.valueOf(strTime));
                         progressBar.setProgress(Integer.valueOf(strTime));
-                        countDownTimer = new CountDownTimer((Integer.valueOf(strTime) * 1000 + 50), 1000) {
-                            int count = 0;
+                        countDownTimer = new CountDownTimer((Integer.valueOf(strTime) * 1000), 100) {
                             public void onTick(long millisUntilFinished) {
                                 Log.v(TAG, "CountDownTimer.onTick(): sUntilFinished: " + (millisUntilFinished) / 1000);
                                 //Log.d(TAG, "CountDownTimer Startwert: " + strTime);
-                                textViewZeit.setText(millisUntilFinished / 1000 + "s");
-                                if (count > 0) {
-                                    progressBar.setProgress((int) (millisUntilFinished / 1000));
-                                }
-                                Log.v(TAG, "counter: " + count);
-                                count++;
+                                textViewZeit.setText(1+(int)(Math.ceil(millisUntilFinished / 1000)) + "s");
+                                    progressBar.setProgress(1+(int) (Math.ceil(millisUntilFinished / 1000)));
                             }
                             public void onFinish() {
                                 Log.v(TAG, "CountDownTimer.onFinish()");
-                                //countDownTimer.cancel();
-                                Log.v(TAG, "counter: " + count);
                                 textViewZeit.setText(0 + "s");
                                 progressBar.setProgress(0);
-                                count = 0;
                                 countDownTimer.cancel();
                                 buttonStart.setText(getString(R.string.button_Alarm));
-                                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.alarm_1);
-                                mediaPlayer.setLooping(true);
                                 mediaPlayer.start();
                             }
                         };
                         countDownTimer.start();
+                        //Disbabled die Zeiteingabe während der Timer läuft
                         editTextStartzeit.setEnabled(false);
                         buttonStart.setText(R.string.button_Stop);
                         Log.d(TAG, "buttonStart(): START ");
+                        //Erstellt Mediaplayer beim drücken des Start Buttons
+                        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.alarm_1);
+                        mediaPlayer.setLooping(true);
                     } else if (getResources().getString(R.string.button_Stop).equals(buttonStart.getText())) {
                         countDownTimer.cancel();
                         buttonStart.setText(R.string.button_Start);
@@ -101,11 +105,17 @@ public class MainActivity extends AppCompatActivity {
                     } else if (getResources().getString(R.string.button_Alarm).equals(buttonStart.getText())) {
                         buttonStart.setText(R.string.button_Start);
                         Log.d(TAG, "buttonStart(): STOP ALARM ");
-                        mediaPlayer.stop();
                         editTextStartzeit.setEnabled(true);
+                        if (mediaPlayer!= null) {
+                            mediaPlayer.stop();
+                            //Löscht ausstehende Befehle an Mediaplayer
+                            mediaPlayer.reset();
+                            //Gibt Resourcen frei, setzt Mediaplayer in Endstate ("gelöscht")
+                            mediaPlayer.release();
+                            mediaPlayer= null;
+                        }
                     }
                 }
-                Log.d(TAG, "buttonStart.OnClickListener()");
             }
         });
     }
